2ea67b4ce1d811dbc91036a70cd936b9ea7df50a
daangelov <a.dragomir1996@gmail.com>
2018-06-25 01:42:59 +0300

    Add functions for power and factirial

---
+++ src/com/pi/MathFunctions.java
@@ -0,0 +1,51 @@
+package com.pi;
+
+import java.math.BigInteger;
+
+class MathFunctions {
+
+    /**
+     * Calculate the power bitwise
+     * http://en.wikipedia.org/w/index.php?title=Modular_exponentiation&oldid=517653653#Right-to-left_binary_method
+     *
+     * @param base     BigInteger
+     * @param exponent BigInteger
+     * @return BigInteger result
+     */
+    static BigInteger power(BigInteger base, BigInteger exponent) {
+
+        BigInteger result = BigInteger.ONE;
+
+        if (exponent.equals(BigInteger.ZERO)) {
+            return BigInteger.ONE;
+        }
+
+        while (exponent.compareTo(BigInteger.ZERO) > 0) {
+            boolean isOdd = exponent.testBit(0);
+            if (isOdd) {
+                result = result.multiply(base);
+            }
+            exponent = exponent.shiftRight(1);
+            base = (base.multiply(base));
+        }
+
+        return result;
+    }
+
+    /**
+     * Calculate n! (n-th factorial)
+     *
+     * @param n BigInteger
+     * @return BigInteger result
+     */
+    static BigInteger factorial(BigInteger n) {
+        BigInteger result = BigInteger.ONE;
+
+        while (!n.equals(BigInteger.ZERO)) {
+            result = result.multiply(n);
+            n = n.subtract(BigInteger.ONE);
+        }
+
+        return result;
+    }
+}
